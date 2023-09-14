b9d4fa0126b72c8e4b3096c5cf417610c9c74b40
sopanha09 <sopanhasous@mail.com>
2023-09-14 20:11:00 +0700

    GeoLocation

---
+++ src/components/UserLocation.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import humidity from "../Image/humidity.png";
+import weatherImages from "../IconList/weatherIcon";
+import wind from "../Image/wind.png";
+import cloudy from "../Image/cloudy.png";
+import "./UserLocation.css"
+
+const api = {
+  key: "6d880005d55b4bfb3ee5c68bcd0b9806",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
+const UserLocation = () => {
+  const [location, setLocation] = useState(null);
+  const [weather, setWeather] = useState({});
+
+  const iconData = "03d";
+  const iconImg = weatherImages[iconData];
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(success, error);
+    } else {
+      console.log("Geolocation not supported");
+    }
+  }, []);
+
+  const success = (position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    setLocation({ latitude, longitude });
+    console.log(`Latitude: ${latitude}, longitude: ${longitude}`);
+
+    fetch(
+      `${api.base}weather?lat=${latitude}&lon=${longitude}&appid=${api.key}&units=metric`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+        console.log(data);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  const error = () => {
+    console.log("Unable to retrieve your location");
+  };
+  return (
+    <div className="weatherToday">
+      {!location ? (
+        <p>Loading location data...</p>
+      ) : (
+        <div>
+          <p>{location.name}</p>
+          {weather && (
+            <div className="container">
+              <div className="weatherImage">
+                <img src={iconImg} alt="" />
+                <div className="humidity-wind">
+                  <div className="humidityWind">
+                    <p>
+                      <img src={humidity} alt="" />
+                      Humidity
+                    </p>
+                    <p>
+                      <img src={wind} alt="" /> Wind
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
+
+          <div className="weatherTitle">
+            <div className="title">
+              <p>Today</p>
+              <p>{weather.name}</p>
+            </div>
+            <div className="weatherMain">
+              {weather.main && weather.main.temp && (
+                <p>Feels like: {weather.main.temp} °C</p>
+              )}
+              {weather.main && weather.main.temp && (
+                <p>{weather.weather[0].main}</p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default UserLocation;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import weatherImages from "../IconList/weatherIcon";
 import humidity from "../Image/humidity.png";
 import wind from "../Image/wind.png";
-
+import UserLocation from "./UserLocation";
 
 export const Weather = () => {
   const [search, setSearch] = useState("");
@@ -11,46 +11,41 @@ export const Weather = () => {
   const iconData = "03d";
   const iconImg = weatherImages[iconData];
 
-  
-
   return (
-  <div className="Weather" >
-    <div className="container">
-      <form className="search" > 
-        <input
-          className="input"
-          type="text"
-          placeholder="Search for city..."
-          onChange={(e) => setSearch(e.target.value)}
-        />
-        <button className="submit" >Search</button>
-      </form>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="weatherToday">
-          <div className="weatherImage">
-            <img src={iconImg} alt="" />
-            <div className="humidity-wind" >
-            <div className="humidityWind" >
-              <p><img src={humidity} alt=""/>Humidity</p>
-              <p><img src={wind} alt="" /> Wind</p>
+    <div className="Weather">
+      <div className="container">
+        <form className="search">
+          <input
+            className="input"
+            type="text"
+            placeholder="Search for city..."
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button className="submit">Search</button>
+        </form>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <div className="weatherToday">
+            <div className="weatherImage">
+              <img src={iconImg} alt="" />
+              <div className="humidity-wind">
+                <div className="humidityWind">
+                  <p>
+                    <img src={humidity} alt="" />
+                    Humidity
+                  </p>
+                  <p>
+                    <img src={wind} alt="" /> Wind
+                  </p>
+                </div>
+              </div>
             </div>
-            </div>
+
+            <UserLocation />
           </div>
-          <div>
-            <div className="title" >
-            <h3>Today</h3>
-            <h2>Phnom Penh</h2>
-            </div>
-            <div className="weatherMain">
-            <h5>Feels like: 28 &deg;C</h5>
-            <h5>Clear Sky</h5>
-            </div>
-          </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
-  </div>
   );
 };
