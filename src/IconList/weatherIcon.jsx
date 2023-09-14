769ea605c58cbf36b003b76e12bdd6c4c2279091
sopanha09 <sopanhasous@mail.com>
2023-09-14 17:30:38 +0700

    Build Layout Weather

---
+++ src/IconList/weatherIcon.jsx
@@ -0,0 +1,36 @@
+import clearSkySun from '../Image/cloudy.png';
+import clearSky from '../Image/ClearSky.png';
+import cloudy from '../Image/cloudy.png';
+import cloud from '../Image/cloud.png';
+import brokenClouds from '../Image/brokenClouds.png';
+import rainy from '../Image/rainy.png';
+import rainyNight from '../Image/rainyNight.png';
+import scatteredCloudsDay from '../Image/scatteredCloudsDay.png';
+import scatteredCloudsNight from '../Image/scatteredCloudsNight.png';
+import storm from '../Image/storm.png';
+import snowflake from '../Image/snowflake.png';
+import wind from '../Image/wind.png';
+
+
+const weatherImages = {
+    "01d": clearSkySun,
+    "01n": clearSky,
+    "02d": cloudy,
+    "02n": cloud,
+    "03d": scatteredCloudsDay,
+    "03n": scatteredCloudsNight,
+    "04d": brokenClouds,
+    "04n": brokenClouds,
+    "09d": rainy,
+    "09n": rainyNight,
+    "10d": rainy,
+    "10n": rainyNight,
+    "11d": storm,
+    "11n": storm,
+    "13d": snowflake,
+    "13n": snowflake,
+    "50d": wind,
+    "50n": wind,
+};
+
+export default weatherImages;
